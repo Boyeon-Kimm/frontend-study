8b8bd67d0ebd01ad6b230d1944a83e8a2e1f5765
Boyeon-Kimm <kkbbyy1113@gmail.com>
2023-06-18 19:11:49 +0900

    0618

---
+++ React/20230618/my-app/src/App.jsx
@@ -0,0 +1,11 @@
+// const App = () => {
+export const App = () => {
+  return (
+    <>
+      {console.log("TEST")}
+      <h1>안녕하세요!</h1>
+      <p>잘 지내시죠?</p>
+      <button>버튼</button>
+    </>
+  );
+}
---
+++ React/20230618/my-app/src/index.js
@@ -0,0 +1,31 @@
+// import { Fragment } from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+
+// const App = () => {
+  // return null;
+  // return <h1>안녕하세요!</h1>;
+
+  // return (
+  //   <div>
+  //     <h1>안녕하세요!</h1>
+  //     <p>잘 지내시죠?</p>
+  //   </div>
+  // );
+
+  // return (
+  //   <Fragment>
+  //     <h1>안녕하세요!</h1>
+  //     <p>잘 지내시죠?</p>
+  //   </Fragment>
+  // );
+
+  // return (
+  //   <>
+  //     <h1>안녕하세요!</h1>
+  //     <p>잘 지내시죠?</p>
+  //   </>
+  // );
+// };
+
+ReactDOM.render(<App />, document.getElementById("root"));
